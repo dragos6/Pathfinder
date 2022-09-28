82942aef37238b72ba66f6b86d33dcbd36ec170b
dragos6 <99660191+dragos6@users.noreply.github.com>
2022-09-29 00:10:27 +0300

    SelftDestruct on Particle

---
+++ Assets/SelfDestruct.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+
+    public float timeLeft = 5f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
